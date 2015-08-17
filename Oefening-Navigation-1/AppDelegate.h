170ad2331f7b980e782bb20e6ca17caf66be3b00
Jonathan Provo <j.provo@easi.net>
2015-08-17 21:59:34 +0200

    Commit files.

---
+++ Oefening-Navigation-1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Oefening-Navigation-1
+//
+//  Created by Jonathan Provo on 17/08/15.
+//  Copyright (c) 2015 EASI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
